e675f0600cbe173cc7ff6fd4588249cc700dea2f
Susana Piñero <susananzth@gmail.com>
2017-08-10 21:06:09 -0400

    initial commit

---
+++ js/javascript.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Susana on 8/3/2017.
+ */
+
+
+    /*Agregando saltos de lineas y comillas en un alert*/
+// var mensaje = "¡Hola Mundo!\n Qué fácil es incluis \'comillas simples\'\n y \"comillas dobles\"";
+
+// alert(mensaje);
+// alert("Soy el segundo alert :)");
+
+    /*Arreglos*/
+// var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto",
+             // "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+    /*Imprimir un arreglo con un while*/
+// var i=0;
+// while (i<=11){
+//    alert(meses[i]);
+//    ++i;
+//}
+
+
