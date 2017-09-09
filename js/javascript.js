30da784c346c9d964f31165ea869bbbbf81d2e2a
Susana Piñero <susananzth@gmail.com>
2017-09-09 13:14:17 -0400

    añadiendo el javascript solo a un documento aparte

--- js/javascript.js
+++ js/javascript.js
@@ -30,6 +30,33 @@ $(document).ready(function(){
 //    ++i;
 //}
 
+     /* Matematica
+    let x = 4;
+    x += 2; // x equals 6
 
+    let y = 4;
+    y -= 2; // y equals 2
 
+    let z = 4;
+    z *= 2; // z equals 8
+
+    let r = 4;
+    r++; // r equals 5
+
+    let t = 4;
+    t--; // t equals 3
+
+      let molecule = 16;
+      let particle = 18;
+      let assay = 3;
+
+      // Add and assign to molecule below
+      molecule += 16;
+
+      // Multiply and assign to particle below
+      particle *=6.02;
+
+      // Increment assay by 1
+      assay++;
+    */
 });
