4df839080e0a6c6338cb4099f7b1fbb0fa5f5e53
Susana Piñero <susananzth@gmail.com>
2017-12-16 20:35:30 -0400

    PDO consultas preparadas sql

--- php/php-partials/partials-mysql-pdo.php
+++ php/php-partials/partials-mysql-pdo.php
@@ -1,7 +1,24 @@
 <?php
+    $name = $_GET["txt-search"];
     try{//Intenta conectar con la DB
         //Construimos un objeto utilizando PDO para colocar los datos de conexión
         $connection = new PDO("mysql:hots=localhost; dbname=curso_php", "root", "");
+        //Nos aseguramos de que no haya error al conectar con la base de datos.
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //Asignamos el tipo de codificación.
+        $connection->exec("SET CHARACTER SET utf8");
+        //Guardamos la sentencia sql en una varible.
+        $sql = "select * from usuarios where nombre = ?";
+        //Creamos una variable para guardar el PFO statement.
+        $result = $connection->prepare($sql);
+        //Aquí le pasamos el parámetro para el sql.
+        $result->execute(array($name));
+        //Aquí recorrerá cada registro de la tabla (statement).
+        while ($register = $result->fetch(PDO::FETCH_ASSOC)){
+            echo "Cédula: " . $register['cedula'] . " -  Nombre: " . $register['nombre'] .
+                " -  Apellido: " . $register['apellido'] . " -  Teléfono: " . $register['telefono'] .
+                " -  Dirección: " . $register['direccion'] . ".";
+        }
         echo "Conexión OK";
     }    catch (Exception $e){ //Si no logra conectarse entonces hará lo siguiente:
         //Mata el proceso y guarda el mensaje de error.
