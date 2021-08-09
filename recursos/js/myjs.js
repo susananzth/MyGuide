8977d08a29ffd53ec44c474493c1295e7ab335f1
Susana Piñero <susananzth@gmail.com>
2021-08-08 20:33:06 -0500

    Agregando enlace de navegación para imágenes

--- recursos/js/myjs.js
+++ recursos/js/myjs.js
@@ -1,4 +1,9 @@
 var nombre = "Susana";
 var altura = 160;
 
-var concatenacion = nombre + ", tu altuna es: " + altuna + " cm.";
+var concatenacion = nombre + ", tu altuna es: " + altura + " cm.";
+// Habilitar tooltips
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
